8a33e07fc78d31563c9ee870725e12a16fe5b7b0
Shadowteach <Lakshandamayantha81@gmail.com>
2024-09-28 11:56:18 +0530

    Update grupcommand.js

--- plugins/grupcommand.js
+++ plugins/grupcommand.js
@@ -97,3 +97,28 @@ reply('*Error !!*')
 l(e)
 }
 })
+
+
+cmd({
+    pattern: "add",
+    desc: "Add a member to the group.",
+    category: "group",
+    react: "🔖",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        if (!isGroup) return reply('This command can only be used in a group❗.')
+        if (!isBotAdmins) return reply('කරුනාකර ශැඩො md බොට් වෙත ඇඩ්මින් ලබා දෙන්න ❗.')
+        if (!isAdmins) return reply('ඔබ owner නොවෙ ❌.')
+
+        const user = q.split(' ')[0]
+        if (!user) return reply('Please provide a phone number to add.')
+
+        await conn.groupParticipantsUpdate(from, [`${user}@s.whatsapp.net`], 'add')
+        await reply(`@${user} has been added to the group.`, { mentions: [`${user}@s.whatsapp.net`] })
+    } catch (e) {
+        console.log(e)
+        reply(`${e}`)
+    }
+})
