497510fa9d4bf846ed7345e342b2bcc332dcbb57
Lakshan Damayantha <Lakshandamayantha81@gmail.com>
2024-10-02 08:18:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -129,7 +129,9 @@ conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://i.img
             mek = mek.messages[0]
             if (!mek.message) return
             mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
-            if (mek.key && mek.key.remoteJid === 'status@broadcast') return
+            if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
+            await conn.readMessages([mek.key]
+            }
             const m = sms(conn, mek)
             const type = getContentType(mek.message)
             const content = JSON.stringify(mek.message)
