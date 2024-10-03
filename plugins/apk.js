3cd742e1e1bcf068e5c6991cc4d980ba41a7a308
Lakshan Damayantha <Lakshandamayantha81@gmail.com>
2024-10-03 10:01:05 +0530

    Update apk.js

--- plugins/apk.js
+++ plugins/apk.js
@@ -1,46 +1,18 @@
-const { cmd, commands } = require('../command');
-const scraper = require("../lib/scraper");
-const axios = require('axios');
-const fetch = require('node-fetch');
-const { fetchJson, getBuffer } = require('../lib/functions');
-const { lookup } = require('mime-types');
-const fs = require('fs');
-const path = require('path');
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+//                                                                                                      //
+//                                    𝗦𝗛𝗔𝗗𝗢𝗪  𝐁𝐎𝐓                                                 //
+//                                                                                                      //
+//                                         Ｖ：6.0                                                       //
 
-//Apk Download
-cmd({
-    pattern: "apk",
-    desc: "Downloads Apk",
-    use: ".apk <app_name>",
-    react: "📥",
-    category: "download",
-    filename: __filename
-},
-async (conn, mek, m, { from, quoted, body, q, reply }) => {
-    const appId = q.trim();
-    if (!appId) return reply(`Please provide an app name for link ❗`);
-    
-    reply("_Downloading Shadow md/ " + appId + "_");
-    
-    try {
-        const appInfo = await scraper.aptoideDl(appId);
-        const buff = await getBuffer(appInfo.link);
-        
-        if (!buff || !appInfo.appname) {
-            return await conn.sendMessage(from, { react: { text: '❌', key: mek.key } });
-        }
-        
-        await conn.sendMessage(
-            from,
-            { document: buff, caption: `ꜱʜᴀᴅᴏᴡ ᴍᴅ ᴡᴀᴛꜱ ᴀᴘᴘ ʙᴏᴛ⚝`, mimetype: "application/vnd.android.package-archive", filename: `${appInfo.appname}.apk` },
-            { quoted: mek }
-        );
-        
-        await conn.sendMessage(from, { react: { text: '✅', key: mek.key } });
-        reply("*_Download Success Shadow md apk ➤_*");
-    } catch (e) {
-        console.log(e);
-        await conn.sendMessage(from, { react: { text: '❌', key: mek.key } });
-        reply(`Error: ${e.message}`);
-    }
-});
+//
+────────────────────────────────────────────────────────────────────────────
+//
+▒█▀▀▀█ ▒█░▒█ ░█▀▀█ ▒█▀▀▄ ▒█▀▀▀█ ▒█░░▒█ 　 ▒█▀▄▀█ ▒█▀▀▄ 
+░▀▀▀▄▄ ▒█▀▀█ ▒█▄▄█ ▒█░▒█ ▒█░░▒█ ▒█▒█▒█ 　 ▒█▒█▒█ ▒█░▒█ 
+▒█▄▄▄█ ▒█░▒█ ▒█░▒█ ▒█▄▄▀ ▒█▄▄▄█ ▒█▄▀▄█ 　 ▒█░░▒█ ▒█▄▄▀
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+//*
+//  * @project_name : SHADOW-MD
+//  * @author : LAKSHAN DAMAYANTHA
+//  * @description : SHADOW ,A Multi-functional whatsapp user bot.
+function _0x5769(_0x44c8d1,_0x36c5dd){const _0x275569=_0x2755();return _0x5769=function(_0x5769b7,_0x186fe3){_0x5769b7=_0x5769b7-0x161;let _0x52056a=_0x275569[_0x5769b7];return _0x52056a;},_0x5769(_0x44c8d1,_0x36c5dd);}const _0x230867=_0x5769;function _0x2755(){const _0x22fcd2=['5rapCoJ','application/vnd.android.package-archive','782154CeeaQe','1096314nBPvzQ','appname','mime-types','Error:\x20','download','681894nlAdkl','Downloads\x20Apk','trim','8147ycqOTo','352611mlFKBx','1127768AMCRzh','Please\x20provide\x20an\x20app\x20name\x20for\x20link\x20❗','ꜱʜᴀᴅᴏᴡ\x20ᴍᴅ\x20ᴡᴀᴛꜱ\x20ᴀᴘᴘ\x20ʙᴏᴛ⚝','*_Download\x20Success\x20Shadow\x20md\x20apk\x20➤_*','key','.apk\x20<app_name>','sendMessage','.apk','../lib/functions','aptoideDl','_Downloading\x20Shadow\x20md/\x20','718176ICUuff','log'];_0x2755=function(){return _0x22fcd2;};return _0x2755();}(function(_0x4f12f6,_0x216db8){const _0x40a8d9=_0x5769,_0x2de353=_0x4f12f6();while(!![]){try{const _0xc2cece=parseInt(_0x40a8d9(0x16c))/0x1+parseInt(_0x40a8d9(0x164))/0x2+parseInt(_0x40a8d9(0x169))/0x3+-parseInt(_0x40a8d9(0x179))/0x4+parseInt(_0x40a8d9(0x161))/0x5*(-parseInt(_0x40a8d9(0x163))/0x6)+-parseInt(_0x40a8d9(0x16d))/0x7+-parseInt(_0x40a8d9(0x16e))/0x8;if(_0xc2cece===_0x216db8)break;else _0x2de353['push'](_0x2de353['shift']());}catch(_0x2c31e4){_0x2de353['push'](_0x2de353['shift']());}}}(_0x2755,0x44ef3));const {cmd,commands}=require('../command'),scraper=require('../lib/scraper'),axios=require('axios'),fetch=require('node-fetch'),{fetchJson,getBuffer}=require(_0x230867(0x176)),{lookup}=require(_0x230867(0x166)),fs=require('fs'),path=require('path');cmd({'pattern':'apk','desc':_0x230867(0x16a),'use':_0x230867(0x173),'react':'📥','category':_0x230867(0x168),'filename':__filename},async(_0x13127c,_0x34ac54,_0x1977c0,{from:_0x6d246f,quoted:_0x46c020,body:_0xe13165,q:_0x8153bc,reply:_0x321e0e})=>{const _0x21cb6c=_0x230867,_0x529197=_0x8153bc[_0x21cb6c(0x16b)]();if(!_0x529197)return _0x321e0e(_0x21cb6c(0x16f));_0x321e0e(_0x21cb6c(0x178)+_0x529197+'_');try{const _0x5df28f=await scraper[_0x21cb6c(0x177)](_0x529197),_0x2ec80b=await getBuffer(_0x5df28f['link']);if(!_0x2ec80b||!_0x5df28f[_0x21cb6c(0x165)])return await _0x13127c[_0x21cb6c(0x174)](_0x6d246f,{'react':{'text':'❌','key':_0x34ac54[_0x21cb6c(0x172)]}});await _0x13127c['sendMessage'](_0x6d246f,{'document':_0x2ec80b,'caption':_0x21cb6c(0x170),'mimetype':_0x21cb6c(0x162),'filename':_0x5df28f[_0x21cb6c(0x165)]+_0x21cb6c(0x175)},{'quoted':_0x34ac54}),await _0x13127c[_0x21cb6c(0x174)](_0x6d246f,{'react':{'text':'✅','key':_0x34ac54[_0x21cb6c(0x172)]}}),_0x321e0e(_0x21cb6c(0x171));}catch(_0x32b2d3){console[_0x21cb6c(0x17a)](_0x32b2d3),await _0x13127c['sendMessage'](_0x6d246f,{'react':{'text':'❌','key':_0x34ac54[_0x21cb6c(0x172)]}}),_0x321e0e(_0x21cb6c(0x167)+_0x32b2d3['message']);}});
