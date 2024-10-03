d407ff31451ec93b94bda141e0d206427edaa218
Lakshan Damayantha <Lakshandamayantha81@gmail.com>
2024-10-03 10:23:05 +0530

    Update fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -1,85 +1,43 @@
-const config = require('../config')
-const getFbVideoInfo = require("fb-downloader-scrapper")
-const {
-    cmd,
-    commands
-} = require('../command')
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+//                                                                                                      //
+//                                    𝗦𝗛𝗔𝗗𝗢𝗪  𝐁𝐎𝐓                                                 //
+//                                                                                                      //
+//                                         Ｖ：6.0                                                       //
 
+//
+//──────────────────────────────────────//──────────────────────────────────────
+//
 
-cmd({
-    pattern: "fb",
-    alias: ["facebook"],
-    use: '.fb https://www.facebook.com/100029474354770/videos/837567391064603/',
-    react: "🎥",
-    desc: 'Download videos from facebook',
-    category: "download",
-    filename: __filename
+░██████╗██╗░░██╗░█████╗░██████╗░░█████╗░░██╗░░░░░░░██╗
+██╔════╝██║░░██║██╔══██╗██╔══██╗██╔══██╗░██║░░██╗░░██║
+╚█████╗░███████║███████║██║░░██║██║░░██║░╚██╗████╗██╔╝
+░╚═══██╗██╔══██║██╔══██║██║░░██║██║░░██║░░████╔═████║░
+██████╔╝██║░░██║██║░░██║██████╔╝╚█████╔╝░░╚██╔╝░╚██╔╝░
+╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░░╚════╝░░░░╚═╝░░░╚═╝░░
 
-},
+███╗░░░███╗██████╗░
+████╗░████║██╔══██╗
+██╔████╔██║██║░░██║
+██║╚██╔╝██║██║░░██║
+██║░╚═╝░██║██████╔╝
+╚═╝░░░░░╚═╝╚═════╝░
 
-    async (conn, m, mek, { from, q, reply }) => {
-        if (!q || !q.includes('facebook.com')) return await reply('*Please enter a valid facebook url!*');
-        const url = q.replace(/\?mibextid=[^&]*/, '');
-        getFbVideoInfo(url)
-            .then((result) => {
-                const msg = `\`✦ 𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞 𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗𝗘𝗥 ✦\`
-`
+██╗░░░██╗███████╗██████╗░░██████╗██╗░█████╗░███╗░░██╗
+██║░░░██║██╔════╝██╔══██╗██╔════╝██║██╔══██╗████╗░██║
+╚██╗░██╔╝█████╗░░██████╔╝╚█████╗░██║██║░░██║██╔██╗██║
+░╚████╔╝░██╔══╝░░██╔══██╗░╚═══██╗██║██║░░██║██║╚████║
+░░╚██╔╝░░███████╗██║░░██║██████╔╝██║╚█████╔╝██║░╚███║
+░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═════╝░╚═╝░╚════╝░╚═╝░░╚══╝
 
-                let buttons = [{
-                    name: "cta_url",
-                    buttonParamsJson: JSON.stringify({
-                        display_text: 'Watch on Facebook',
-                        url: q,
-                        merchant_url: q
-                    }),
-                },
-                {
-                    name: "quick_reply",
-                    buttonParamsJson: JSON.stringify({
-                        display_text: "SD Quality",
-                        id: ".downfb " + result.sd
-                    }),
-                },
-                {
-                    name: "quick_reply",
-                    buttonParamsJson: JSON.stringify({
-                        display_text: "HD Quality",
-                        id: ".downfb " + result.hd
-                    }),
-                }
-                ]
-                let message = {
-                    image: result.thumbnail,
-                    header: '',
-                    footer: config.FOOTER,
-                    body: msg
-
-                }
-                return conn.sendButtonMessage(from, buttons, m, message)
-            }).catch((err) => {
-                console.log(err)
-            })
-
-
-    });
-
-
-cmd({
-    pattern: "downfb",
-    react: "🎥",
-    dontAddCommandList: true,
-    filename: __filename
-},
-
-    async (conn, mek, m, { from, q, reply }) => {
-        try {
-            if (!q) return await reply('*Not Found!*')
-
-            await conn.sendMessage(from, { video: { url: q } }, { quoted: mek })
-            await conn.sendMessage(from, { react: { text: '✅', key: mek.key } })
-
-        } catch (e) {
-            reply('*Error !!*')
-            console.log(e)
-        }
-    })
+░█████╗░██╗░░░██╗
+██╔═══╝░██║░░░██║
+██████╗░╚██╗░██╔╝
+██╔══██╗░╚████╔╝░
+╚█████╔╝░░╚██╔╝░░
+░╚════╝░░░░╚═╝░░░
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+//*
+//  * @project_name : SHADOW-MD
+//  * @author : LAKSHAN DAMAYANTHA
+//  * @description : SHADOW ,A Multi-functional whatsapp user bot.
+function _0x329c(){const _0x367fff=['fb-downloader-scrapper','facebook.com','.downfb\x20','facebook','replace','2085224TQaDUb','Download\x20videos\x20from\x20facebook','sendMessage','sendButtonMessage','cta_url','catch','2dSDqut','stringify','138705QgDZrP','1421812RPFpSj','1110OeYxIk','key','20005FVYAXD','includes','528681NDCUHA','FOOTER','then','*Please\x20enter\x20a\x20valid\x20facebook\x20url!*','thumbnail','quick_reply','4YFfEAZ','download','*Error\x20!!*','23490CSQLCi','HD\x20Quality','1098810LEaMyb','*Not\x20Found!*'];_0x329c=function(){return _0x367fff;};return _0x329c();}const _0x4e2eae=_0x20c1;(function(_0x5a8704,_0x4f2c94){const _0x370734=_0x20c1,_0x372796=_0x5a8704();while(!![]){try{const _0x393844=parseInt(_0x370734(0x174))/0x1+-parseInt(_0x370734(0x172))/0x2*(parseInt(_0x370734(0x17a))/0x3)+parseInt(_0x370734(0x180))/0x4*(parseInt(_0x370734(0x178))/0x5)+-parseInt(_0x370734(0x165))/0x6+-parseInt(_0x370734(0x175))/0x7+parseInt(_0x370734(0x16c))/0x8+parseInt(_0x370734(0x183))/0x9*(parseInt(_0x370734(0x176))/0xa);if(_0x393844===_0x4f2c94)break;else _0x372796['push'](_0x372796['shift']());}catch(_0x32c692){_0x372796['push'](_0x372796['shift']());}}}(_0x329c,0x1fe1f));const config=require('../config'),getFbVideoInfo=require(_0x4e2eae(0x167)),{cmd,commands}=require('../command');function _0x20c1(_0x366742,_0x44eb2e){const _0x329c4a=_0x329c();return _0x20c1=function(_0x20c194,_0x3b6a56){_0x20c194=_0x20c194-0x165;let _0x32fdb6=_0x329c4a[_0x20c194];return _0x32fdb6;},_0x20c1(_0x366742,_0x44eb2e);}cmd({'pattern':'fb','alias':[_0x4e2eae(0x16a)],'use':'.fb\x20https://www.facebook.com/100029474354770/videos/837567391064603/','react':'🎥','desc':_0x4e2eae(0x16d),'category':_0x4e2eae(0x181),'filename':__filename},async(_0x4f6a9a,_0x7826a0,_0x13fdf0,{from:_0x371156,q:_0x48d966,reply:_0x2d8ecd})=>{const _0x876473=_0x4e2eae;if(!_0x48d966||!_0x48d966[_0x876473(0x179)](_0x876473(0x168)))return await _0x2d8ecd(_0x876473(0x17d));const _0x6f1295=_0x48d966[_0x876473(0x16b)](/\?mibextid=[^&]*/,'');getFbVideoInfo(_0x6f1295)[_0x876473(0x17c)](_0x24813e=>{const _0x506755=_0x876473,_0x4ef76c='`✦\x20𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞\x20𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗𝗘𝗥\x20✦`\x0a\x0a';let _0x12c316=[{'name':_0x506755(0x170),'buttonParamsJson':JSON['stringify']({'display_text':'Watch\x20on\x20Facebook','url':_0x48d966,'merchant_url':_0x48d966})},{'name':_0x506755(0x17f),'buttonParamsJson':JSON[_0x506755(0x173)]({'display_text':'SD\x20Quality','id':_0x506755(0x169)+_0x24813e['sd']})},{'name':_0x506755(0x17f),'buttonParamsJson':JSON[_0x506755(0x173)]({'display_text':_0x506755(0x184),'id':_0x506755(0x169)+_0x24813e['hd']})}],_0x560f06={'image':_0x24813e[_0x506755(0x17e)],'header':'','footer':config[_0x506755(0x17b)],'body':_0x4ef76c};return _0x4f6a9a[_0x506755(0x16f)](_0x371156,_0x12c316,_0x7826a0,_0x560f06);})[_0x876473(0x171)](_0x55762d=>{console['log'](_0x55762d);});}),cmd({'pattern':'downfb','react':'🎥','dontAddCommandList':!![],'filename':__filename},async(_0x132bb8,_0x516996,_0x21f9f8,{from:_0x175b51,q:_0x79df86,reply:_0x190b29})=>{const _0xee1fb9=_0x4e2eae;try{if(!_0x79df86)return await _0x190b29(_0xee1fb9(0x166));await _0x132bb8[_0xee1fb9(0x16e)](_0x175b51,{'video':{'url':_0x79df86}},{'quoted':_0x516996}),await _0x132bb8[_0xee1fb9(0x16e)](_0x175b51,{'react':{'text':'✅','key':_0x516996[_0xee1fb9(0x177)]}});}catch(_0x55d38d){_0x190b29(_0xee1fb9(0x182)),console['log'](_0x55d38d);}});
