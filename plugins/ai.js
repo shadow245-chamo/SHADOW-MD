e2794fa9fb045a7f55e2433edbc5e47b839b5dbb
Lakshan Damayantha <Lakshandamayantha81@gmail.com>
2024-10-03 09:55:45 +0530

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -1,117 +1,19 @@
-const config = require('../config')
-const {cmd , commands} = require('../command')
-const { fetchJson } = require('../lib/functions')
-cmd({
-    pattern: "ai",
-    react: "🧠",
-    desc: "ai chat",
-    category: "main",
-    filename: __filename
-},
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-let data = await fetchJson(`https://chatgptforprabath-md.vercel.app/api/gptv1?q=${q}`)
-return reply(`${data.data}`)
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
-})
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+//                                                                                                      //
+//                                    𝗦𝗛𝗔𝗗𝗢𝗪  𝐁𝐎𝐓                                                 //
+//                                                                                                      //
+//                                         Ｖ：6.0                                                       //
 
-cmd({
-    pattern: "genimg",
-    desc: "Ai image.",
-    react: "🧠",
-    category: "other",
-    filename: __filename
-},
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
+//
+────────────────────────────────────────────────────────────────────────────
+//
+▒█▀▀▀█ ▒█░▒█ ░█▀▀█ ▒█▀▀▄ ▒█▀▀▀█ ▒█░░▒█ 　 ▒█▀▄▀█ ▒█▀▀▄ 
+░▀▀▀▄▄ ▒█▀▀█ ▒█▄▄█ ▒█░▒█ ▒█░░▒█ ▒█▒█▒█ 　 ▒█▒█▒█ ▒█░▒█ 
+▒█▄▄▄█ ▒█░▒█ ▒█░▒█ ▒█▄▄▀ ▒█▄▄▄█ ▒█▄▀▄█ 　 ▒█░░▒█ ▒█▄▄▀
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+//*
+//  * @project_name : SHADOW-MD
+//  * @author : LAKSHAN DAMAYANTHA
+//  * @description : SHADOW ,A Multi-functional whatsapp user bot.
 
-await conn.sendMessage(from,{image :{ url: `https://api.vihangayt.com/ai/stablediff?prompt=${q}&negprompt=not%20hd,%20watermark` },caption: '> *SHADOW MD BY LAKSHAN TEACH TANKS MANUL MD genimg command 👨‍💻💗➤*' },{quoted:mek});
-
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
-})
-
-//======Wallpaper=====
-
-cmd({
-    pattern: "gemini",
-    desc: "Ai chat.",
-    react: "🧠",
-    category: "other",
-    filename: __filename
-},
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-
-let res = await fetchJson(`https://api.vihangayt.com/ai/gemini?q=${q}`)
-//----------------------------------
-let data = res.data
-
-await reply(data.response)
-
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
-})
-
-//======gpt4=======
-
-cmd({
-    pattern: "gpt",
-    desc: "Ai chat.",
-    react: "🧠",
-    category: "other",
-    filename: __filename
-},
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-
-let data = await fetchJson(`https://api.vihangayt.com/ai/gpt?q=${q}`)
-return reply(`${data.data}`)
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
-})
-
-cmd({
-    pattern: "lamda",
-    desc: "Ai chat.",
-    react: "🧠",
-    category: "other",
-    filename: __filename
-},
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-
-let data = await fetchJson(`https://api.vihangayt.com/ai/lamda?q=${q}`)
-return reply(`${data.data}`)
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
-})
-
-cmd({
-    pattern: "lalaland",
-    desc: "Ai chat.",
-    react: "🧠",
-    category: "other",
-    filename: __filename
-},
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-
-let data = await fetchJson(`https://api.vihangayt.com/ai/lalaland?q=${q}`)
-return reply(`${data.data}`)
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
-})
+const _0x1f9766=_0x5ccc;function _0x1636(){const _0x5250f1=['9ZMnBsU','lalaland','response','8181732ubFGfq','Ai\x20image.','6120505BdjCzx','1492370xTKbCG','log','genimg','https://api.vihangayt.com/ai/lalaland?q=','9uCFOrY','1034980IYQENB','https://api.vihangayt.com/ai/stablediff?prompt=','>\x20*SHADOW\x20MD\x20BY\x20LAKSHAN\x20TEACH\x20TANKS\x20MANUL\x20MD\x20genimg\x20command\x20👨‍💻💗➤*','152wQtiah','1008406gXDsNM','lamda','sendMessage','Ai\x20chat.','ai\x20chat','6LWzyzJ','2593030YAIVpP','12868NfCDwR','gpt','&negprompt=not%20hd,%20watermark','https://api.vihangayt.com/ai/gemini?q=','other','24qktDqs','main','data','https://api.vihangayt.com/ai/lamda?q='];_0x1636=function(){return _0x5250f1;};return _0x1636();}(function(_0x3bd18e,_0x3cef1d){const _0xaa00f3=_0x5ccc,_0x1d27c0=_0x3bd18e();while(!![]){try{const _0x22cb32=parseInt(_0xaa00f3(0xae))/0x1*(-parseInt(_0xaa00f3(0xc5))/0x2)+parseInt(_0xaa00f3(0xb7))/0x3*(-parseInt(_0xaa00f3(0xc2))/0x4)+-parseInt(_0xaa00f3(0xbc))/0x5*(-parseInt(_0xaa00f3(0xac))/0x6)+-parseInt(_0xaa00f3(0xc6))/0x7*(-parseInt(_0xaa00f3(0xb3))/0x8)+parseInt(_0xaa00f3(0xc1))/0x9*(-parseInt(_0xaa00f3(0xbd))/0xa)+parseInt(_0xaa00f3(0xad))/0xb+parseInt(_0xaa00f3(0xba))/0xc;if(_0x22cb32===_0x3cef1d)break;else _0x1d27c0['push'](_0x1d27c0['shift']());}catch(_0x288ada){_0x1d27c0['push'](_0x1d27c0['shift']());}}}(_0x1636,0xa3aa8));const config=require('../config'),{cmd,commands}=require('../command'),{fetchJson}=require('../lib/functions');function _0x5ccc(_0x51bf19,_0x296576){const _0x163638=_0x1636();return _0x5ccc=function(_0x5ccc40,_0x35be61){_0x5ccc40=_0x5ccc40-0xa8;let _0x4e1771=_0x163638[_0x5ccc40];return _0x4e1771;},_0x5ccc(_0x51bf19,_0x296576);}cmd({'pattern':'ai','react':'🧠','desc':_0x1f9766(0xab),'category':_0x1f9766(0xb4),'filename':__filename},async(_0x584b67,_0x4243d5,_0x2aa643,{from:_0x3875d7,quoted:_0x28e4b6,body:_0x480f0d,isCmd:_0x129deb,command:_0xd81bc6,args:_0x8d6a31,q:_0x204d4f,isGroup:_0x2ed88f,sender:_0x1c4400,senderNumber:_0x5e8389,botNumber2:_0x52d8e8,botNumber:_0x114579,pushname:_0xb4902f,isMe:_0x133ccc,isOwner:_0x4416dc,groupMetadata:_0x3992be,groupName:_0x5dc999,participants:_0x403373,groupAdmins:_0x206111,isBotAdmins:_0x2bbaec,isAdmins:_0x16fc10,reply:_0x3ea678})=>{const _0x18e2e8=_0x1f9766;try{let _0x36fbd4=await fetchJson('https://chatgptforprabath-md.vercel.app/api/gptv1?q='+_0x204d4f);return _0x3ea678(''+_0x36fbd4[_0x18e2e8(0xb5)]);}catch(_0xd8828a){console[_0x18e2e8(0xbe)](_0xd8828a),_0x3ea678(''+_0xd8828a);}}),cmd({'pattern':_0x1f9766(0xbf),'desc':_0x1f9766(0xbb),'react':'🧠','category':_0x1f9766(0xb2),'filename':__filename},async(_0x6efb22,_0x21417d,_0x52a501,{from:_0x22b969,quoted:_0x517e15,body:_0x474b76,isCmd:_0x32c8ee,command:_0x20a094,args:_0xd8cfd5,q:_0x1f3c61,isGroup:_0x368c8f,sender:_0x2cdb6c,senderNumber:_0x262789,botNumber2:_0x56d995,botNumber:_0x243ee1,pushname:_0x24821c,isMe:_0x4263d1,isOwner:_0x2b59f7,groupMetadata:_0x3fed45,groupName:_0x1e6366,participants:_0x25f0b3,groupAdmins:_0x45d3e0,isBotAdmins:_0x1e462,isAdmins:_0x56c53b,reply:_0x41323b})=>{const _0x2c9ef3=_0x1f9766;try{await _0x6efb22[_0x2c9ef3(0xa9)](_0x22b969,{'image':{'url':_0x2c9ef3(0xc3)+_0x1f3c61+_0x2c9ef3(0xb0)},'caption':_0x2c9ef3(0xc4)},{'quoted':_0x21417d});}catch(_0x1f647c){console[_0x2c9ef3(0xbe)](_0x1f647c),_0x41323b(''+_0x1f647c);}}),cmd({'pattern':'gemini','desc':_0x1f9766(0xaa),'react':'🧠','category':'other','filename':__filename},async(_0x58f895,_0x9dfdd6,_0x25200e,{from:_0x1b7e95,quoted:_0x5dfcd0,body:_0x6d854f,isCmd:_0x511fb0,command:_0x3c9803,args:_0x4381c2,q:_0x295f96,isGroup:_0x26ffbf,sender:_0x382039,senderNumber:_0x2c4fe3,botNumber2:_0x11c750,botNumber:_0x74ae50,pushname:_0x5d0ef2,isMe:_0x2a617a,isOwner:_0x15fbd2,groupMetadata:_0x5a38a6,groupName:_0xe1792e,participants:_0x4408db,groupAdmins:_0x5f313d,isBotAdmins:_0x41f6ca,isAdmins:_0x2b3380,reply:_0x396ea9})=>{const _0x481307=_0x1f9766;try{let _0x3795c0=await fetchJson(_0x481307(0xb1)+_0x295f96),_0x3d8f97=_0x3795c0[_0x481307(0xb5)];await _0x396ea9(_0x3d8f97[_0x481307(0xb9)]);}catch(_0x33a1e8){console['log'](_0x33a1e8),_0x396ea9(''+_0x33a1e8);}}),cmd({'pattern':_0x1f9766(0xaf),'desc':_0x1f9766(0xaa),'react':'🧠','category':_0x1f9766(0xb2),'filename':__filename},async(_0x49d87f,_0x1c9606,_0xb4cecd,{from:_0x3291cb,quoted:_0xb296b3,body:_0x5362a5,isCmd:_0x22fa12,command:_0xecf38e,args:_0xb14693,q:_0x491e77,isGroup:_0x487f2a,sender:_0x50ccbe,senderNumber:_0x206088,botNumber2:_0x8afdfe,botNumber:_0x506f09,pushname:_0x466e18,isMe:_0x33f8c4,isOwner:_0x5cbe8e,groupMetadata:_0xa654,groupName:_0x3d0318,participants:_0x1635ff,groupAdmins:_0x4e8526,isBotAdmins:_0x3d5392,isAdmins:_0x58a649,reply:_0x19217c})=>{const _0x458108=_0x1f9766;try{let _0x5eb46c=await fetchJson('https://api.vihangayt.com/ai/gpt?q='+_0x491e77);return _0x19217c(''+_0x5eb46c[_0x458108(0xb5)]);}catch(_0x348ee5){console['log'](_0x348ee5),_0x19217c(''+_0x348ee5);}}),cmd({'pattern':_0x1f9766(0xa8),'desc':'Ai\x20chat.','react':'🧠','category':_0x1f9766(0xb2),'filename':__filename},async(_0x163725,_0x2afe22,_0x24a526,{from:_0x51a232,quoted:_0x169ca6,body:_0x5ea176,isCmd:_0x3b854d,command:_0x3c7436,args:_0x419a93,q:_0x7c6e59,isGroup:_0x32febc,sender:_0x5f3113,senderNumber:_0x73e985,botNumber2:_0x17b6fb,botNumber:_0x49b2fd,pushname:_0x1bf3f7,isMe:_0x1fbae3,isOwner:_0xd641ac,groupMetadata:_0x17074d,groupName:_0x591fc9,participants:_0x5dada8,groupAdmins:_0x3eda95,isBotAdmins:_0x1ba3d6,isAdmins:_0x2798a6,reply:_0x586170})=>{const _0x4f5e3c=_0x1f9766;try{let _0x27236f=await fetchJson(_0x4f5e3c(0xb6)+_0x7c6e59);return _0x586170(''+_0x27236f[_0x4f5e3c(0xb5)]);}catch(_0x45e321){console['log'](_0x45e321),_0x586170(''+_0x45e321);}}),cmd({'pattern':_0x1f9766(0xb8),'desc':'Ai\x20chat.','react':'🧠','category':'other','filename':__filename},async(_0x5ec97c,_0x1829d0,_0x987240,{from:_0x18f070,quoted:_0x203c80,body:_0x1a8a0c,isCmd:_0x3ac7c0,command:_0x3f31b5,args:_0x365fe8,q:_0x41e98b,isGroup:_0x1f07e9,sender:_0x53754e,senderNumber:_0x191f25,botNumber2:_0xd41007,botNumber:_0x3a0984,pushname:_0x1dc223,isMe:_0x35bb10,isOwner:_0x3410a8,groupMetadata:_0x16e387,groupName:_0xbe825e,participants:_0x198c4e,groupAdmins:_0x1309f7,isBotAdmins:_0x1d32bf,isAdmins:_0x36dc37,reply:_0xd96bb4})=>{const _0x3bc4f6=_0x1f9766;try{let _0x105a9e=await fetchJson(_0x3bc4f6(0xc0)+_0x41e98b);return _0xd96bb4(''+_0x105a9e[_0x3bc4f6(0xb5)]);}catch(_0x3db3e4){console['log'](_0x3db3e4),_0xd96bb4(''+_0x3db3e4);}});
