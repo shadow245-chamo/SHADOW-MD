b748025bdae8849d9381884fd372c492c3d44f6b
Lakshan Damayantha <Lakshandamayantha81@gmail.com>
2024-10-03 10:17:49 +0530

    Update download.js

--- plugins/download.js
+++ plugins/download.js
@@ -1,83 +1,43 @@
-const { fetchJson } = require('../lib/functions')
-const config = require('../config')
-const { cmd, commands } = require('../command')
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+//                                                                                                      //
+//                                    𝗦𝗛𝗔𝗗𝗢𝗪  𝐁𝐎𝐓                                                 //
+//                                                                                                      //
+//                                         Ｖ：6.0                                                       //
 
-// FETCH API URL
-let baseUrl;
-(async () => {
-    let baseUrlGet = await fetchJson(`https://raw.githubusercontent.com/prabathLK/PUBLIC-URL-HOST-DB/main/public/url.json`)
-    baseUrl = baseUrlGet.api
-})();
+//
+//──────────────────────────────────────//──────────────────────────────────────
+//
 
+░██████╗██╗░░██╗░█████╗░██████╗░░█████╗░░██╗░░░░░░░██╗
+██╔════╝██║░░██║██╔══██╗██╔══██╗██╔══██╗░██║░░██╗░░██║
+╚█████╗░███████║███████║██║░░██║██║░░██║░╚██╗████╗██╔╝
+░╚═══██╗██╔══██║██╔══██║██║░░██║██║░░██║░░████╔═████║░
+██████╔╝██║░░██║██║░░██║██████╔╝╚█████╔╝░░╚██╔╝░╚██╔╝░
+╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░░╚════╝░░░░╚═╝░░░╚═╝░░
 
-const yourName = "*ꜱʜᴀᴅᴏᴡ ᴍᴅ ᴏᴡɴᴇʀ ʙʏ ʟᴀᴋꜱʜᴀɴ ᴛᴇᴀᴄʜ ➤*";
+███╗░░░███╗██████╗░
+████╗░████║██╔══██╗
+██╔████╔██║██║░░██║
+██║╚██╔╝██║██║░░██║
+██║░╚═╝░██║██████╔╝
+╚═╝░░░░░╚═╝╚═════╝░
 
-//twitter dl (x)
-cmd({
-    pattern: "twitter",
-    alias: ["twdl"],
-    desc: "download tw videos",
-    react: "📁",
-    category: "download",
-    filename: __filename
-},
-async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
-    try {
-        if (!q && !q.startsWith("https://")) return reply("give me twitter url 💭")
-        //fetch data from api  
-        let data = await fetchJson(`${baseUrl}/api/twitterdl?url=${q}`)
-        reply("*Downloading Shadow md twdl..➤*")
-        //send video (hd,sd)
-        await conn.sendMessage(from, { video: { url: data.data.data.HD }, mimetype: "video/mp4", caption: `- HD\n\n ${yourName}` }, { quoted: mek })
-        await conn.sendMessage(from, { video: { url: data.data.data.SD }, mimetype: "video/mp4", caption: `- SD \n\n ${yourName}` }, { quoted: mek })  
-        //send audio    
-        await conn.sendMessage(from, { audio: { url: data.data.data.audio }, mimetype: "audio/mpeg" }, { quoted: mek })  
-    } catch (e) {
-        console.log(e)
-        reply(`${e}`)
-    }
-})
+██╗░░░██╗███████╗██████╗░░██████╗██╗░█████╗░███╗░░██╗
+██║░░░██║██╔════╝██╔══██╗██╔════╝██║██╔══██╗████╗░██║
+╚██╗░██╔╝█████╗░░██████╔╝╚█████╗░██║██║░░██║██╔██╗██║
+░╚████╔╝░██╔══╝░░██╔══██╗░╚═══██╗██║██║░░██║██║╚████║
+░░╚██╔╝░░███████╗██║░░██║██████╔╝██║╚█████╔╝██║░╚███║
+░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═════╝░╚═╝░╚════╝░╚═╝░░╚══╝
 
-//gdrive(google drive) dl
-cmd({
-    pattern: "gdrive",
-    alias: ["googledrive"],
-    react: "📁",
-    desc: "download gdrive files",
-    category: "download",
-    filename: __filename
-},
-async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
-    try {
-        if (!q && !q.startsWith("https://")) return reply("give me gdrive url 💭")
-        //fetch data from api  
-        let data = await fetchJson(`${baseUrl}/api/gdrivedl?url=${q}`)
-        reply("*Downloading Shadow md gdrive..➢*")
-        await conn.sendMessage(from, { document: { url: data.data.download }, fileName: data.data.fileName, mimetype: data.data.mimeType, caption: `${data.data.fileName}\n\n${yourName}` }, { quoted: mek })                                                                                                                 
-    } catch (e) {
-        console.log(e)
-        reply(`${e}`)
-    }
-})
-
-//mediafire dl
-cmd({
-    pattern: "mediafire",
-    alias: ["mfire"],
-    react: "📁",
-    desc: "download mfire files",
-    category: "download",
-    filename: __filename
-},
-async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
-    try {
-        if (!q && !q.startsWith("https://")) return reply("give me mediafire url 💭")
-        //fetch data from api  
-        let data = await fetchJson(`${baseUrl}/api/mediafiredl?url=${q}`)
-        reply("*Downloading Shadow md mfire ➤..*")
-        await conn.sendMessage(from, { document: { url: data.data.link_1 }, fileName: data.data.name, mimetype: data.data.file_type, caption: `${data.data.name}\n\n${yourName}` }, { quoted: mek })                                                                                                                 
-    } catch (e) {
-        console.log(e)
-        reply(`${e}`)
-    }
-})
+░█████╗░██╗░░░██╗
+██╔═══╝░██║░░░██║
+██████╗░╚██╗░██╔╝
+██╔══██╗░╚████╔╝░
+╚█████╔╝░░╚██╔╝░░
+░╚════╝░░░░╚═╝░░░
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+//*
+//  * @project_name : SHADOW-MD
+//  * @author : LAKSHAN DAMAYANTHA
+//  * @description : SHADOW ,A Multi-functional whatsapp user bot.
+const _0x5609e4=_0x18af;(function(_0x3e8403,_0x538202){const _0x210b5c=_0x18af,_0x4cace4=_0x3e8403();while(!![]){try{const _0x3842bd=-parseInt(_0x210b5c(0xa3))/0x1*(-parseInt(_0x210b5c(0xb7))/0x2)+parseInt(_0x210b5c(0xb1))/0x3+parseInt(_0x210b5c(0xb2))/0x4+-parseInt(_0x210b5c(0xbc))/0x5*(-parseInt(_0x210b5c(0xca))/0x6)+-parseInt(_0x210b5c(0xc7))/0x7+-parseInt(_0x210b5c(0xc1))/0x8*(-parseInt(_0x210b5c(0xbd))/0x9)+-parseInt(_0x210b5c(0xc0))/0xa*(parseInt(_0x210b5c(0xb5))/0xb);if(_0x3842bd===_0x538202)break;else _0x4cace4['push'](_0x4cace4['shift']());}catch(_0x5e99e6){_0x4cace4['push'](_0x4cace4['shift']());}}}(_0x1580,0x34c53));const {fetchJson}=require(_0x5609e4(0xbb)),config=require(_0x5609e4(0xad)),{cmd,commands}=require(_0x5609e4(0xa6));let baseUrl;((async()=>{let _0x508518=await fetchJson('https://raw.githubusercontent.com/prabathLK/PUBLIC-URL-HOST-DB/main/public/url.json');baseUrl=_0x508518['api'];})());function _0x18af(_0x69dbd8,_0x25b300){const _0x158012=_0x1580();return _0x18af=function(_0x18afab,_0xbfcc05){_0x18afab=_0x18afab-0x9f;let _0x687fce=_0x158012[_0x18afab];return _0x687fce;},_0x18af(_0x69dbd8,_0x25b300);}const yourName=_0x5609e4(0xa2);function _0x1580(){const _0x3ea818=['-\x20SD\x20\x0a\x0a\x20','give\x20me\x20gdrive\x20url\x20💭','../lib/functions','15580hkGHVu','9kRxBEZ','twdl','download\x20gdrive\x20files','49330HIwqti','499784psXAme','name','download\x20tw\x20videos','https://','mfire','*Downloading\x20Shadow\x20md\x20mfire\x20➤..*','1928584lDZFOK','*Downloading\x20Shadow\x20md\x20gdrive..➢*','give\x20me\x20mediafire\x20url\x20💭','30tDTucp','/api/mediafiredl?url=','startsWith','audio/mpeg','*ꜱʜᴀᴅᴏᴡ\x20ᴍᴅ\x20ᴏᴡɴᴇʀ\x20ʙʏ\x20ʟᴀᴋꜱʜᴀɴ\x20ᴛᴇᴀᴄʜ\x20➤*','13256KOQMcZ','link_1','sendMessage','../command','give\x20me\x20twitter\x20url\x20💭','download','gdrive','video/mp4','fileName','file_type','../config','data','*Downloading\x20Shadow\x20md\x20twdl..➤*','/api/gdrivedl?url=','207459WQDfnB','390088wgacrE','log','mediafire','11QbcRex','googledrive','38OjiuWf','download\x20mfire\x20files'];_0x1580=function(){return _0x3ea818;};return _0x1580();}cmd({'pattern':'twitter','alias':[_0x5609e4(0xbe)],'desc':_0x5609e4(0xc3),'react':'📁','category':'download','filename':__filename},async(_0x4cf1a4,_0x514ffb,_0x4a3863,{from:_0x5dfef3,quoted:_0x3a9d35,body:_0x7484e3,isCmd:_0x3c5726,command:_0x19236a,args:_0x1d78cf,q:_0x460594,isGroup:_0x7dd904,sender:_0x2c30ee,senderNumber:_0x56776d,botNumber2:_0x284704,botNumber:_0x4d5ac7,pushname:_0x37455b,isMe:_0x10b5e2,isOwner:_0xccca90,groupMetadata:_0x36908e,groupName:_0x13b9e5,participants:_0x39e083,groupAdmins:_0x3d0509,isBotAdmins:_0x4d0393,isAdmins:_0x35978a,reply:_0x5efb})=>{const _0x4d9217=_0x5609e4;try{if(!_0x460594&&!_0x460594[_0x4d9217(0xa0)](_0x4d9217(0xc4)))return _0x5efb(_0x4d9217(0xa7));let _0x4171dc=await fetchJson(baseUrl+'/api/twitterdl?url='+_0x460594);_0x5efb(_0x4d9217(0xaf)),await _0x4cf1a4['sendMessage'](_0x5dfef3,{'video':{'url':_0x4171dc[_0x4d9217(0xae)][_0x4d9217(0xae)]['HD']},'mimetype':_0x4d9217(0xaa),'caption':'-\x20HD\x0a\x0a\x20'+yourName},{'quoted':_0x514ffb}),await _0x4cf1a4[_0x4d9217(0xa5)](_0x5dfef3,{'video':{'url':_0x4171dc[_0x4d9217(0xae)][_0x4d9217(0xae)]['SD']},'mimetype':_0x4d9217(0xaa),'caption':_0x4d9217(0xb9)+yourName},{'quoted':_0x514ffb}),await _0x4cf1a4[_0x4d9217(0xa5)](_0x5dfef3,{'audio':{'url':_0x4171dc[_0x4d9217(0xae)][_0x4d9217(0xae)]['audio']},'mimetype':_0x4d9217(0xa1)},{'quoted':_0x514ffb});}catch(_0x3d48f5){console[_0x4d9217(0xb3)](_0x3d48f5),_0x5efb(''+_0x3d48f5);}}),cmd({'pattern':_0x5609e4(0xa9),'alias':[_0x5609e4(0xb6)],'react':'📁','desc':_0x5609e4(0xbf),'category':_0x5609e4(0xa8),'filename':__filename},async(_0x24acba,_0x3b6f59,_0x4dcaa5,{from:_0x301f58,quoted:_0x423b4a,body:_0x3aac74,isCmd:_0x4a6973,command:_0x2c7143,args:_0x77cbcc,q:_0x2894d2,isGroup:_0x3e5fb5,sender:_0x14650b,senderNumber:_0x25f976,botNumber2:_0x4f90bc,botNumber:_0x5b73aa,pushname:_0x1cad4f,isMe:_0x5ed45f,isOwner:_0x587c58,groupMetadata:_0x46bdb9,groupName:_0x1b404d,participants:_0x331721,groupAdmins:_0x316199,isBotAdmins:_0x28ecc4,isAdmins:_0x26be25,reply:_0x417cbf})=>{const _0x28cc67=_0x5609e4;try{if(!_0x2894d2&&!_0x2894d2[_0x28cc67(0xa0)](_0x28cc67(0xc4)))return _0x417cbf(_0x28cc67(0xba));let _0x182b5e=await fetchJson(baseUrl+_0x28cc67(0xb0)+_0x2894d2);_0x417cbf(_0x28cc67(0xc8)),await _0x24acba[_0x28cc67(0xa5)](_0x301f58,{'document':{'url':_0x182b5e[_0x28cc67(0xae)][_0x28cc67(0xa8)]},'fileName':_0x182b5e[_0x28cc67(0xae)][_0x28cc67(0xab)],'mimetype':_0x182b5e['data']['mimeType'],'caption':_0x182b5e[_0x28cc67(0xae)]['fileName']+'\x0a\x0a'+yourName},{'quoted':_0x3b6f59});}catch(_0x33bf2b){console[_0x28cc67(0xb3)](_0x33bf2b),_0x417cbf(''+_0x33bf2b);}}),cmd({'pattern':_0x5609e4(0xb4),'alias':[_0x5609e4(0xc5)],'react':'📁','desc':_0x5609e4(0xb8),'category':_0x5609e4(0xa8),'filename':__filename},async(_0x240d6b,_0x1e7a32,_0x5b8118,{from:_0x575c47,quoted:_0x1567b5,body:_0x56a90c,isCmd:_0x7c8d59,command:_0x279a8b,args:_0x5051df,q:_0x2cfcdc,isGroup:_0x123472,sender:_0x1ab133,senderNumber:_0x1f0d14,botNumber2:_0x1189e7,botNumber:_0x4044a0,pushname:_0x593d8f,isMe:_0x42bc3c,isOwner:_0x5749aa,groupMetadata:_0x542540,groupName:_0x2e4538,participants:_0x17ec1c,groupAdmins:_0x1580ed,isBotAdmins:_0x921ba7,isAdmins:_0x4e5f00,reply:_0x3170ba})=>{const _0x15ac48=_0x5609e4;try{if(!_0x2cfcdc&&!_0x2cfcdc[_0x15ac48(0xa0)](_0x15ac48(0xc4)))return _0x3170ba(_0x15ac48(0xc9));let _0x577248=await fetchJson(baseUrl+_0x15ac48(0x9f)+_0x2cfcdc);_0x3170ba(_0x15ac48(0xc6)),await _0x240d6b['sendMessage'](_0x575c47,{'document':{'url':_0x577248[_0x15ac48(0xae)][_0x15ac48(0xa4)]},'fileName':_0x577248[_0x15ac48(0xae)]['name'],'mimetype':_0x577248[_0x15ac48(0xae)][_0x15ac48(0xac)],'caption':_0x577248[_0x15ac48(0xae)][_0x15ac48(0xc2)]+'\x0a\x0a'+yourName},{'quoted':_0x1e7a32});}catch(_0x52b0a3){console['log'](_0x52b0a3),_0x3170ba(''+_0x52b0a3);}});
