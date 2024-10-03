5f9e33cba29f8823d26c338d52f79ff11579ef9e
Lakshan Damayantha <Lakshandamayantha81@gmail.com>
2024-10-03 09:59:06 +0530

    Update anime.js

--- plugins/anime.js
+++ plugins/anime.js
@@ -1,22 +1,18 @@
-const axios = require('axios');
-const { cmd, commands } = require('../command');
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+//                                                                                                      //
+//                                    𝗦𝗛𝗔𝗗𝗢𝗪  𝐁𝐎𝐓                                                 //
+//                                                                                                      //
+//                                         Ｖ：6.0                                                       //
 
-cmd({
-    pattern: "animegirl",
-    desc: "Fetch a random anime girl image.",
-    category: "fun",
-    react: "👧",
-    filename: __filename
-},
-async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
-    try {
-        const apiUrl = `https://api.waifu.pics/sfw/waifu`;
-        const response = await axios.get(apiUrl);
-        const data = response.data;
-
-        await conn.sendMessage(from, { image: { url: data.url }, caption: '👧 *Random Anime Girl Image* 👧\n>  ❯❯ ꜱʜᴀᴅᴏᴡ-ᴍᴅ ᴡᴀᴛꜱ ᴀᴘᴘ ʙᴏᴛ ➣' }, { quoted: mek });
-    } catch (e) {
-        console.log(e);
-        reply(`*Error Fetching Anime Girl image*: ${e.message}`);
-    }
-});
+//
+────────────────────────────────────────────────────────────────────────────
+//
+▒█▀▀▀█ ▒█░▒█ ░█▀▀█ ▒█▀▀▄ ▒█▀▀▀█ ▒█░░▒█ 　 ▒█▀▄▀█ ▒█▀▀▄ 
+░▀▀▀▄▄ ▒█▀▀█ ▒█▄▄█ ▒█░▒█ ▒█░░▒█ ▒█▒█▒█ 　 ▒█▒█▒█ ▒█░▒█ 
+▒█▄▄▄█ ▒█░▒█ ▒█░▒█ ▒█▄▄▀ ▒█▄▄▄█ ▒█▄▀▄█ 　 ▒█░░▒█ ▒█▄▄▀
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+//*
+//  * @project_name : SHADOW-MD
+//  * @author : LAKSHAN DAMAYANTHA
+//  * @description : SHADOW ,A Multi-functional whatsapp user bot.
+const _0x1d58cf=_0x4cc4;(function(_0x21f8db,_0xc9f38f){const _0x5083cb=_0x4cc4,_0x31a7d1=_0x21f8db();while(!![]){try{const _0x512e3b=-parseInt(_0x5083cb(0x17c))/0x1+-parseInt(_0x5083cb(0x176))/0x2+-parseInt(_0x5083cb(0x173))/0x3*(-parseInt(_0x5083cb(0x172))/0x4)+parseInt(_0x5083cb(0x177))/0x5+parseInt(_0x5083cb(0x17f))/0x6*(-parseInt(_0x5083cb(0x175))/0x7)+parseInt(_0x5083cb(0x179))/0x8*(parseInt(_0x5083cb(0x174))/0x9)+parseInt(_0x5083cb(0x171))/0xa;if(_0x512e3b===_0xc9f38f)break;else _0x31a7d1['push'](_0x31a7d1['shift']());}catch(_0x39196a){_0x31a7d1['push'](_0x31a7d1['shift']());}}}(_0x5b4f,0x8e748));const axios=require('axios'),{cmd,commands}=require(_0x1d58cf(0x17a));function _0x4cc4(_0x29b39f,_0x568d88){const _0x5b4f89=_0x5b4f();return _0x4cc4=function(_0x4cc465,_0xe08575){_0x4cc465=_0x4cc465-0x170;let _0x5976f3=_0x5b4f89[_0x4cc465];return _0x5976f3;},_0x4cc4(_0x29b39f,_0x568d88);}cmd({'pattern':'animegirl','desc':_0x1d58cf(0x178),'category':'fun','react':'👧','filename':__filename},async(_0x4e22f3,_0x45c152,_0x3c79ae,{from:_0x4dc51f,quoted:_0x2b6083,body:_0x411caf,isCmd:_0x295b7f,command:_0x537edb,args:_0x2d7729,q:_0x5cfacf,isGroup:_0x5a95bc,sender:_0x3a9457,senderNumber:_0x3c039e,botNumber2:_0x56587b,botNumber:_0x43879c,pushname:_0x27250d,isMe:_0x31e88f,isOwner:_0xde8512,groupMetadata:_0x38b454,groupName:_0x5f2d15,participants:_0x3cfa6c,groupAdmins:_0x20ff23,isBotAdmins:_0x487f3f,isAdmins:_0x3492c0,reply:_0x55ef0d})=>{const _0x1f42a0=_0x1d58cf;try{const _0x4f12cd=_0x1f42a0(0x17e),_0x5be80e=await axios['get'](_0x4f12cd),_0x302819=_0x5be80e['data'];await _0x4e22f3['sendMessage'](_0x4dc51f,{'image':{'url':_0x302819[_0x1f42a0(0x17b)]},'caption':'👧\x20*Random\x20Anime\x20Girl\x20Image*\x20👧\x0a>\x20\x20❯❯\x20ꜱʜᴀᴅᴏᴡ-ᴍᴅ\x20ᴡᴀᴛꜱ\x20ᴀᴘᴘ\x20ʙᴏᴛ\x20➣'},{'quoted':_0x45c152});}catch(_0x1eca3b){console[_0x1f42a0(0x17d)](_0x1eca3b),_0x55ef0d(_0x1f42a0(0x170)+_0x1eca3b['message']);}});function _0x5b4f(){const _0x57fd45=['3207799DBZvfe','1879448oDLQkL','2035385rsCIJd','Fetch\x20a\x20random\x20anime\x20girl\x20image.','8bDfGVd','../command','url','572478cnnVMh','log','https://api.waifu.pics/sfw/waifu','12UKLNbL','*Error\x20Fetching\x20Anime\x20Girl\x20image*:\x20','18028520uxbjkx','2556204jjcRjN','3lxqzhr','1469088nMPoqi'];_0x5b4f=function(){return _0x57fd45;};return _0x5b4f();}
