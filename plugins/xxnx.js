730702e67855ce0d89c24a41181f0251c18b2315
Lakshan Damayantha <Lakshandamayantha81@gmail.com>
2024-10-06 09:15:11 +0530

    Update xxnx.js

--- plugins/xxnx.js
+++ plugins/xxnx.js
@@ -1,48 +1,44 @@
-const { cmd, commands } = require('../command');
-const scraper = require("../lib/scraper");
-const xnxx = require("xnxx-dl");
-const axios = require('axios');
-const fetch = require('node-fetch');
-const { fetchJson, getBuffer } = require('../lib/functions');
-const path = require('path');
-const fs = require('fs');
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+//                                                                                                      //
+//                                   // 𝗦𝗛𝗔𝗗𝗢𝗪  𝐁𝐎𝐓                                                 //
+//                                                                                                      //
+//                                         Ｖ：6.0                                                       //
 
-const premiumGroups = [];
+//
+//──────────────────────────────────────//──────────────────────────────────────
+//
 
-cmd({
-    pattern: "xnxx",
-    desc: "Downloads a video from XNXX",
-    use: ".xnxx <search_term>",
-    react: "🤤",
-    category: "premium",
-    filename: __filename
-},
-async (conn, mek, m, { from, quoted, body, q, reply }) => {
+//░██████╗██╗░░██╗░█████╗░██████╗░░█████╗░░██╗░░░░░░░██╗
+//██╔════╝██║░░██║██╔══██╗██╔══██╗██╔══██╗░██║░░██╗░░██║
+//╚█████╗░███████║███████║██║░░██║██║░░██║░╚██╗████╗██╔╝
+//░╚═══██╗██╔══██║██╔══██║██║░░██║██║░░██║░░████╔═████║░
+//██████╔╝██║░░██║██║░░██║██████╔╝╚█████╔╝░░╚██╔╝░╚██╔╝░
+//╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░░╚════╝░░░░╚═╝░░░╚═╝░░
 
-    if (!premiumGroups.includes(from)) {
-        return reply("This command is available in premium groups only.");
-    }
-    const searchTerm = q.trim();
-    if (!searchTerm) return reply(`Please provide a search term`);
+//███╗░░░███╗██████╗░
+//████╗░████║██╔══██╗
+//██╔████╔██║██║░░██║
+//██║╚██╔╝██║██║░░██║
+//██║░╚═╝░██║██████╔╝
+//╚═╝░░░░░╚═╝╚═════╝░
 
-    reply(`Searching For Your Video whaite...`);
-    try {
-        const videoInfo = await xnxx.download(searchTerm);        
-        if (!videoInfo || !videoInfo.link_dl) {
-            return await conn.sendMessage(from, { react: { text: '❌', key: mek.key } });
-        }
-        reply(`Downloading video...`);
-        const videoUrl = videoInfo.link_dl;
-        await conn.sendMessage(
-            from,
-            { video : { url: videoUrl }, caption: '*Queen Spriky MD*', mimetype: 'video/mp4' }, { quoted: mek }
-        );
+//██╗░░░██╗███████╗██████╗░░██████╗██╗░█████╗░███╗░░██╗
+//██║░░░██║██╔════╝██╔══██╗██╔════╝██║██╔══██╗████╗░██║
+//╚██╗░██╔╝█████╗░░██████╔╝╚█████╗░██║██║░░██║██╔██╗██║
+//░╚████╔╝░██╔══╝░░██╔══██╗░╚═══██╗██║██║░░██║██║╚████║
+//░░╚██╔╝░░███████╗██║░░██║██████╔╝██║╚█████╔╝██║░╚███║
+//░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═════╝░╚═╝░╚════╝░╚═╝░░╚══╝
 
-        await conn.sendMessage(from, { react: { text: '✅', key: mek.key } });
+//░█████╗░██╗░░░██╗
+//██╔═══╝░██║░░░██║
+//██████╗░╚██╗░██╔╝
+//██╔══██╗░╚████╔╝░
+//╚█████╔╝░░╚██╔╝░░
+//░╚════╝░░░░╚═╝░░░
+//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+//*
+//  * @project_name : SHADOW-MD
+//  * @author : LAKSHAN DAMAYANTHA
+//  * @description : SHADOW ,A Multi-functional whatsapp user bot.
 
-    } catch (e) {
-        console.log(e);
-        await conn.sendMessage(from, { react: { text: '❌', key: mek.key } });
-        reply(`Error: ${e.message}`);
-    }
-});
+function _0x20c8(_0x546b2e,_0x5ccfa5){const _0x13d377=_0x13d3();return _0x20c8=function(_0x20c8f3,_0x51c06c){_0x20c8f3=_0x20c8f3-0xd2;let _0x6dad42=_0x13d377[_0x20c8f3];return _0x6dad42;},_0x20c8(_0x546b2e,_0x5ccfa5);}const _0x1c659b=_0x20c8;(function(_0x468f32,_0x1ba7bb){const _0x49aaed=_0x20c8,_0x4c5ff8=_0x468f32();while(!![]){try{const _0x3aee50=-parseInt(_0x49aaed(0xec))/0x1*(-parseInt(_0x49aaed(0xe9))/0x2)+parseInt(_0x49aaed(0xda))/0x3+parseInt(_0x49aaed(0xd2))/0x4*(parseInt(_0x49aaed(0xf2))/0x5)+-parseInt(_0x49aaed(0xdf))/0x6*(-parseInt(_0x49aaed(0xde))/0x7)+parseInt(_0x49aaed(0xd3))/0x8+parseInt(_0x49aaed(0xe2))/0x9*(parseInt(_0x49aaed(0xea))/0xa)+-parseInt(_0x49aaed(0xd9))/0xb;if(_0x3aee50===_0x1ba7bb)break;else _0x4c5ff8['push'](_0x4c5ff8['shift']());}catch(_0x4d5159){_0x4c5ff8['push'](_0x4c5ff8['shift']());}}}(_0x13d3,0x6296b));const {cmd,commands}=require(_0x1c659b(0xe4)),scraper=require(_0x1c659b(0xd4)),xnxx=require(_0x1c659b(0xe0)),axios=require('axios'),fetch=require(_0x1c659b(0xee)),{fetchJson,getBuffer}=require(_0x1c659b(0xe7)),path=require(_0x1c659b(0xe3)),fs=require('fs'),premiumGroups=[];function _0x13d3(){const _0x17d1da=['../lib/functions','log','14820lsLGUw','10uEHMJF','message','83aiNMxQ','trim','node-fetch','premium','Downloading\x20video...','*Queen\x20Spriky\x20MD*','20525sGFMyf','72UbFjno','2511864DHattI','../lib/scraper','.xnxx\x20<search_term>','Error:\x20','sendMessage','download','14336696WvAICu','857322VZHMah','video/mp4','xnxx','Downloads\x20a\x20video\x20from\x20XNXX','21eVippq','401538SYJyOX','xnxx-dl','includes','1959381nuOJaX','path','../command','link_dl','key'];_0x13d3=function(){return _0x17d1da;};return _0x13d3();}cmd({'pattern':_0x1c659b(0xdc),'desc':_0x1c659b(0xdd),'use':_0x1c659b(0xd5),'react':'🤤','category':_0x1c659b(0xef),'filename':__filename},async(_0x538348,_0x57656d,_0x3ffb58,{from:_0x36c4d2,quoted:_0x42ba35,body:_0xdb0728,q:_0x2e5c3c,reply:_0x1cf13c})=>{const _0x5523f5=_0x1c659b;if(!premiumGroups[_0x5523f5(0xe1)](_0x36c4d2))return _0x1cf13c('This\x20command\x20is\x20available\x20in\x20premium\x20groups\x20only.');const _0x1dfcf3=_0x2e5c3c[_0x5523f5(0xed)]();if(!_0x1dfcf3)return _0x1cf13c('Please\x20provide\x20a\x20search\x20term');_0x1cf13c('Searching\x20For\x20Your\x20Video\x20whaite...');try{const _0x36a75a=await xnxx[_0x5523f5(0xd8)](_0x1dfcf3);if(!_0x36a75a||!_0x36a75a['link_dl'])return await _0x538348[_0x5523f5(0xd7)](_0x36c4d2,{'react':{'text':'❌','key':_0x57656d['key']}});_0x1cf13c(_0x5523f5(0xf0));const _0x4fddf5=_0x36a75a[_0x5523f5(0xe5)];await _0x538348[_0x5523f5(0xd7)](_0x36c4d2,{'video':{'url':_0x4fddf5},'caption':_0x5523f5(0xf1),'mimetype':_0x5523f5(0xdb)},{'quoted':_0x57656d}),await _0x538348[_0x5523f5(0xd7)](_0x36c4d2,{'react':{'text':'✅','key':_0x57656d[_0x5523f5(0xe6)]}});}catch(_0x3ae55e){console[_0x5523f5(0xe8)](_0x3ae55e),await _0x538348[_0x5523f5(0xd7)](_0x36c4d2,{'react':{'text':'❌','key':_0x57656d['key']}}),_0x1cf13c(_0x5523f5(0xd6)+_0x3ae55e[_0x5523f5(0xeb)]);}});
